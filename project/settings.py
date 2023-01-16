ca06960b14a0d22cb0adbc612d4d10be7bf8edb2
parkergeorge13 <parkergeorge13@gmail.com>
2023-01-16 16:45:56 -0700

    first deployment

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['web-production-cead.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-cead.up.railway.app']
 
 
 # Application definition
